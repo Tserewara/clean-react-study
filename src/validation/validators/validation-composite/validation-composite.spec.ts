380fd82bf8f18d219e1727f9d87e07521fdaaa70
Tserewara <alvaro.alf2@gmail.com>
2022-04-15 14:14:26 -0300

    refactor: add index file to export mocks

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,6 +1,6 @@
-import { FieldValidationSpy } from '@/validation/test/mock-field-validation'
-import faker from '@faker-js/faker'
+import { FieldValidationSpy } from '@/validation/test'
 import { ValidationComposite } from './validation-composite'
+import faker from '@faker-js/faker'
 
 type SutTypes = {
   sut: ValidationComposite
